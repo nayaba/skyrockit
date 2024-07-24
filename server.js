eed62f0c7477dc17e3304c4221853009ce39e6e8
Nabila Ayaba <nabilaayaba@gmail.com>
2024-07-24 16:30:42 +0300

    Add stylesheet

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+const path = require('path')
 
 const isSignedIn = require('./middleware/is-signed-in.js')
 const passUserToView = require('./middleware/pass-user-to-view.js')
@@ -34,6 +35,9 @@ app.use(
   })
 );
 app.use(passUserToView)
+
+// LINK TO PUBLIC DIRECTORY
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   if (req.session.user) {

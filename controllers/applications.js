ddf4823c17db41a2ad8896c6780274d5ad7626e5
Denis S Dujota <denisdujota@gmail.com>
2024-07-25 14:38:36 +0300

    finish edit, add, delete actions for applications

--- controllers/applications.js
+++ controllers/applications.js
@@ -1,12 +1,16 @@
+/* eslint-disable prefer-destructuring */
 const express = require('express');
 
 const router = express.Router();
 
 const User = require('../models/user');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    res.render('applications/index.ejs');
+    const currentUser = await User.findById(req.session.user._id);
+    const applications = currentUser.applications;
+
+    res.render('applications/index.ejs', { applications });
   } catch (error) {
     console.log(error);
     res.redirect('/');
@@ -15,6 +19,58 @@ router.get('/', (req, res) => {
 
 router.get('/new', async (req, res) => {
   res.render('applications/new.ejs');
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+
+    req.body.date = new Date(req.body.date);
+    currentUser.applications.push(req.body);
+
+    await currentUser.save();
+
+    res.redirect(`/users/${currentUser._id}/applications`);
+  } catch (err) {
+    console.error(err);
+
+    res.redirect('/');
+  }
+});
+
+router.get('/:applicationId', async (req, res, next) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const applications = currentUser.applications;
+
+    // in order to pull one specific object from the
+    // embedded array, we need to use the id method
+    // which will call arr.find id === req.params.id
+    res.render('applications/show.ejs', {
+      application: applications.id(req.params.applicationId),
+    });
+  } catch (err) {
+    console.error(err);
+    res.redirect('/');
+  }
+});
+
+router.delete('/:applicationId', async (req, res, next) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+
+    const application = currentUser.applications.id(req.params.applicationId);
+
+    application.deleteOne();
+
+    await currentUser.save();
+
+    res.redirect('/');
+  } catch (error) {
+    console.log(error);
+
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
--- models/user.js
+++ models/user.js
@@ -1,16 +1,22 @@
 const mongoose = require('mongoose');
 
-const applicationSchema = new mongoose.Schema({
-  company: String,
-  title: String,
-  date: Date,
-  notes: String,
-  postingLink: String,
-  status: {
-    type: String,
-    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted']
-  }
-}, { timestamps: true })
+const applicationSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: true,
+    },
+    title: String,
+    date: Date,
+    notes: String,
+    postingLink: String,
+    status: {
+      type: String,
+      enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],
+    },
+  },
+  { timestamps: true }
+);
 
 const userSchema = mongoose.Schema({
   username: {
@@ -21,7 +27,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
-  applications: [applicationSchema]
+  applications: [applicationSchema],
 });
 
 const User = mongoose.model('User', userSchema);

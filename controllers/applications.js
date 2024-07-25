7f7ab08e94d39d9d192f5ce88784ecc42395f0c0
Nabila Ayaba <nabilaayaba@gmail.com>
2024-07-25 14:22:34 +0300

    Implement the Create operation in a POST route.

--- controllers/applications.js
+++ controllers/applications.js
@@ -16,4 +16,17 @@ router.get('/new', async (req, res) => {
     res.render('applications/new.ejs')
 })
 
+router.post('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id)
+        req.body.date = new Date(req.body.date)
+        currentUser.applications.push(req.body)
+        await currentUser.save()
+        res.redirect(`/users/${currentUser._id}/applications`)
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+})
+
 module.exports = router
